a0708819bccd6350d17f21bd34d572a72603f165
Sebastian Weisgerber <weisgerber@cs.uni-saarland.de>
2017-04-10 16:15:33 +0200

    Added Apache 2.0 Licence Header

--- app/src/main/java/saarland/cispa/artist/codelib/CodeLib.java
+++ app/src/main/java/saarland/cispa/artist/codelib/CodeLib.java
@@ -1,3 +1,24 @@
+/**
+ * The ARTist Project (https://artist.cispa.saarland)
+ *
+ * Copyright (C) 2017 CISPA (https://cispa.saarland), Saarland University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author "Oliver Schranz <oliver.schranz@cispa.saarland>"
+ * @author "Sebastian Weisgerber <weisgerber@cispa.saarland>"
+ *
+ */
 package saarland.cispa.artist.codelib;
 
 import android.util.Log;
@@ -26,7 +47,7 @@ public class CodeLib {
      * => Forbidden Class Initialisation (Singleton)
       */
     private CodeLib() {
-        Log.v(TAG, "CodeLib() " + VERSION);
+        Log.v(TAG, TAG + " CodeLib() " + VERSION);
     }
 
     /** Get the name of the calling method
@@ -120,5 +141,23 @@ public class CodeLib {
      */
     public void logLong(final long value) {
         Log.d(TAG, "logLong()    #1: " + value);
+    }
+
+    /**
+     * Logs the paramenter to the android logger.
+     *
+     * @param value parameter
+     */
+    public void logFloat(final float value) {
+        Log.d(TAG, "logFloat()   #1: " + value);
+    }
+
+    /**
+     * Logs the paramenter to the android logger.
+     *
+     * @param value parameter
+     */
+    public void logDouble(final double value) {
+        Log.d(TAG, "logDouble()  #1: " + value);
     }
 }
